355f7fa250d68bd761a6f895b5c6bd332c9e7864
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-03-26 12:52:35 -0400

    build: move to port 8088

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 8080
+EXPOSE 8088
 
 CMD ["npm", "start"]
--- app/routes/erddap/da.js
+++ app/routes/erddap/da.js
@@ -70,7 +70,7 @@ router.get("/coordinates", cacheMiddleware, async (req, res) => {
  *
  */
 router.get("/samples", cacheMiddleware, async (req, res) => {
-  const coordinates = mcache.get(`__express__/erddap/da/coordinates`) ?? [];
+  const coordinates = mcache.get(`__express__/erddap/da/coordinates`) ?? await getCoordinates();
   let data = await getSamples(coordinates);
   res.send(data);
 });
