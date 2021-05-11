4320499bfc570be5d6797da413c38d31de3a3abd
mauradriscoll <51652231+mauradriscoll@users.noreply.github.com>
2021-05-11 12:49:27 -0400

    feat: 🎸 Updated temperature stats

--- app/routes/erddap/fish.js
+++ app/routes/erddap/fish.js
@@ -36,10 +36,24 @@ const getMetrics = async (coordinates) => {
   const sites = coordinates.map((row) => row.station_name);
   const rawData_temp = await getFishData({ sites, datasetId: TEMP_DATASET_ID});
   const rawData_etc = await getFishData({ sites, datasetId: YSI_DATASET_ID});
-  const temp_table = aq.from(rawData_temp)
-  const etc_table = aq.from(rawData_etc)
-  const join = temp_table.join_full(etc_table)
-  return join.objects()
+  var i;
+  let temp_table = {"Fox Island":[], "Whale Rock": [] }
+  for (i = 1959; i < 2018; i++){
+    let year_i = i.toString()
+    let dt_fi = aq.from(rawData_temp).params({year: year_i}).filter((d,$) => (op.includes(d.time, $.year) && d.Station == "Fox Island"))
+    dt_fi = dt_fi.rollup({avg_st: d => op.mean(d.Surface_Temperature), avg_bt: d => op.mean(d.Bottom_Temperature)})
+    dt_fi = dt_fi.derive({year :`d => ${year_i}`})
+
+    let dt_wr = aq.from(rawData_temp).params({year: year_i}).filter((d,$) => (op.includes(d.time, $.year) && d.Station == "Whale Rock"))
+    dt_wr = dt_wr.rollup({avg_st: d => op.mean(d.Surface_Temperature), avg_bt: d => op.mean(d.Bottom_Temperature)})
+    dt_wr = dt_wr.derive({year :`d => ${year_i}`})
+   
+    temp_table["Fox Island"].push(dt_fi.objects()[0])
+    temp_table["Whale Rock"].push(dt_wr.objects()[0])
+    
+  }
+
+  return temp_table
 };
 
 // ROUTES
